26163207dd5d4b250ad1086796012e65567126db
Paulo <paulo.eduardo@codeminer42.com>
2021-08-18 12:15:44 -0300

    Implements pending tests

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Logger do
       it "don't insert anything in output buffer" do
         buffer = instance_double("IO", :<< => nil)
         logger = Logger.new Levels::DEBUG, buffer
+
         logger.log
 
         expect(buffer).to_not have_received(:<<)
@@ -17,8 +18,8 @@ RSpec.describe Logger do
       it "inserts log in output buffer" do
         buffer = instance_double("IO", :<< => nil)
         logger = Logger.new Levels::DEBUG, buffer
-        logger.log "Hello"
 
+        logger.log "Hello"
         expect(buffer).to have_received(:<<)
       end
 
@@ -33,9 +34,43 @@ RSpec.describe Logger do
         expect(buffer).to have_received(:<<).exactly(5).times
       end
 
-      it "logs with correct level label" 
-      it "logs correct timestamp"
-      it "calls #inspec method in inputs"
+      it "logs with correct message" do
+        message = "Hello"
+        buffer = instance_double("IO", :<< => nil)
+        logger = Logger.new Levels::DEBUG, buffer
+
+        logger.log message
+        expect(buffer).to have_received(:<<).with /#{message}/
+      end
+
+      it "calls #to_s method in inputs" do
+        input = instance_double("String", :to_s => "Hello")
+        buffer = instance_double("IO", :<< => nil)
+        logger = Logger.new Levels::DEBUG, buffer
+
+        logger.log input
+        expect(input).to have_received(:to_s)
+      end
+
+      it "logs with correct level label" do
+        Levels.constants.each do |level_name|
+          level = Levels.const_get(level_name)
+          buffer = instance_double("IO", :<< => nil)
+          logger = Logger.new level, buffer
+
+          logger.log "Hello"
+          expect(buffer).to have_received(:<<).with /#{level_name}/i
+        end
+      end
+
+      it "logs correct timestamp" do
+        buffer = instance_double("IO", :<< => nil)
+        logger = Logger.new Levels::DEBUG, buffer
+        date_matcher = /\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\]/ #[2021-08-18 12:00:00]
+        
+        logger.log "Hello"
+        expect(buffer).to have_received(:<<).with date_matcher
+      end
     end
   end
 end
