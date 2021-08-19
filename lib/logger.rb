f8d05e6efa66e02e1eb31684368647d27fe35033
Paulo <paulo.eduardo@codeminer42.com>
2021-08-19 17:50:22 -0300

    Adds #config tests and implementation

--- lib/logger.rb
+++ lib/logger.rb
@@ -6,20 +6,33 @@ module Conq
     def initialize(level, buffer)
       @level = level
       @buffer = buffer
+      @log_format = "[%{DATE} %{TIME}] %{LEVEL}: %{MESSAGE}"
     end
 
     def log(*input)
-      formatted_date = Time.now.strftime("%Y-%M-%d %H:%M:%S")
+      now = Time.now
+      formatted_date = now.strftime("%Y-%M-%d")
+      formatted_time = now.strftime("%H:%M:%S")
 
-      input.each do |data|
-        @buffer << "[#{formatted_date}] #{@level.to_s}: #{data.to_s}\n"
+      
+      input.each do |message|
+        values = {
+          :DATE => formatted_date,
+          :TIME => formatted_time,
+          :LEVEL => @level,
+          :MESSAGE => message
+        }
+
+        @buffer << (@log_format % values) + "\n"
       end
     end
 
     def config(configuration)
       raise TypeError, "Unexpected type of 'configuration'" unless configuration.is_a? Hash
 
-      # To Do
+      @level = configuration[:level] if configuration[:level]
+      @buffer = configuration[:output] if configuration[:output]
+      @log_format = configuration[:format] if configuration[:format]
     end
   end
 end
