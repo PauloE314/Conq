7f070e5bf57bbb8a94f5a29869d7722faaf4d4ec
Paulo <paulo.eduardo@codeminer42.com>
2021-08-20 11:47:56 -0300

    Bug fix

--- lib/conq.rb
+++ lib/conq.rb
@@ -1,2 +1,15 @@
 require_relative "./levels.rb"
 require_relative "./logger.rb"
+
+module Conq
+  @logger = nil
+
+  def self.init(output)
+    @logger = Logger.new output
+  end
+
+  def self.get_global
+    @logger
+  end
+
+end
