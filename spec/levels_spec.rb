28472cbf7e2cdc41b3faaddcfe0e17d869d7f8b0
Paulo <paulo.eduardo@codeminer42.com>
2021-08-20 18:24:43 -0300

    Adds Levels tests

---
+++ spec/levels_spec.rb
@@ -0,0 +1,24 @@
+Levels = Conq::Levels
+
+RSpec.describe Levels do
+  let(:levels) { Levels.constants.map {|level_name| Levels.const_get(level_name) } }
+
+  it "contains all required levels" do
+    expected_levels = [:DEBUG, :INFO, :WARNING, :ERROR, :CRITICAL]
+
+    expect(Levels.constants).to include(*expected_levels)
+  end
+
+  it "have the correct importance order" do
+    sorted_levels = levels.sort
+    expected_sorted_levels = [
+      Levels::DEBUG,
+      Levels::INFO,
+      Levels::WARNING,
+      Levels::ERROR,
+      Levels::CRITICAL
+    ]
+
+    expect(sorted_levels).to be_eql(expected_sorted_levels)
+  end
+end
